148de195d1d963a5fbbeb3fcbfdd1cfcf35eaf62
Dahlia <rlally89@gmail.com>
2022-10-19 17:00:22 -0700

    added in inquirer, created prompts, generated markdown

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# professional-readme-generator
-A professional README generator for the developer in need
+# tester
+![MIT badge](https://img.shields.io/badge/License-MIT-blue)
+
+## Description
+
+none
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contribution](#contribution)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+```
+npm
+```
+
+## Usage
+
+none
+
+## License
+    
+  This project is licensed by the MIT license.
+
+## Contribution 
+
+they cannot
+
+## Tests
+
+To test, run the following:
+
+```
+npm
+```
+
+## Questions
+
+If you have additional questions, reach me at rlally89.  To view more of my work, visit [rachlally](https://github.com/rachlally/).
+
---
+++ index.js
@@ -0,0 +1,74 @@
+// TODO: Include packages needed for this application
+const fs = require("fs")
+const inquirer = require("inquirer")
+const path = require("path")
+const generateMarkdown = require("./utils/generateMarkdown")
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "github"
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is your project's name?",
+        name: "project"
+    },
+    {
+        type: "input",
+        message: "Please write a short description of your project:",
+        name: "description"
+    },
+    {
+        type: "list",
+        message: "What type of license would you like?",
+        name: "license", 
+        choices: ["MIT", "Apache2.0", "GPL3.0", "BSD3", "None"]
+    },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "What command should be used to run tests?",
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: "What are the guidelines for using this repo?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "How can a user contribute to the repo?",
+        name: "contribution"
+    },
+
+];
+
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(),fileName),data);
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt (questions).then((inquirerResponses)=>{
+        console.log("We are generating your README.");
+        writeToFile("README.md", generateMarkdown({...inquirerResponses}))
+    })
+}
+
+// Function call to initialize app
+init();
